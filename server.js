7589ce288ed9847a1a1bdbaf43a9da8a2dc45488
Shriram Rajaraman <32219070+shriram2712@users.noreply.github.com>
2018-02-28 21:24:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
+var Pool = require('pg').Pool;
+
+var config = {
+    user : 'u2015shriramrajaraman' ,
+    database : 'u2015shriramrajaraman' ,
+    host : 'db.imad.hasura-app.io',
+    port : '5432' ,
+    password : process.env.DB_PASSWORD
+}
 var path = require('path');
 
 var app = express();
@@ -73,6 +82,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+app.get('/test-db', function (req, res){
+    //make a select request
+    //return a response
+    pool.query('SELECT * FROM test', function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    }
+    });
+});
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
