2109238255719efcee01eac668f34ea75d7f0c5a
Shriram Rajaraman <32219070+shriram2712@users.noreply.github.com>
2018-03-07 21:11:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,39 @@ function moveRight() {
 img.onclick = function() {
     var interval = setInterval(moveRight,1,50);
 };
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //cAPTURE THE RESPONSE AND STORE IT IN A VARIABLE
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200)
+            {
+                alert('logged in successfully');
+            } else if (request.status === 403)
+            {
+                alert('Username/Password incorrect');
+            } else (request.status === 500)
+            {
+                alert('Something went wrong on the server');
+            }
+        }
+    };
+    
+    //Make the request
+    var username = document.getElementByid('username').value;
+    var password = document.getElementByid('password').value;
+    console.log(username);
+    console.log(password);
+    
+    request.open('POST', 'http://u2015shriramrajaraman.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.setRequestHeader('Content-type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+    
+    
+};
+    
